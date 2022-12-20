51ca1b2ddb8ee399d8e52eb277534ceb7eb895ef
Alexis Campomanes <alexiscp.developer@gmail.com>
2022-12-19 20:18:33 -0500

    maquetado 5.0

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -3,18 +3,10 @@ import './Body.scss';
 import '../Body/Left/Left'
 import Left from '../Body/Left/Left';
 
-
-
-
 const dataFixed = {
-    
   nombre: "",
   description: "",
-  
 }
-
-
-
 
 const Body = () => {
 
@@ -22,12 +14,10 @@ const Body = () => {
   const [todos, setTodos] = useState([]);
 
   const handleSubmit = (evt) => {
-        
     evt.preventDefault();
-
     const newTodo = {
       id: crypto.randomUUID(),
-      nombre:values.nombre,
+      nombre: values.nombre,
       description: values.description,
     };
     setTodos([...todos, newTodo]);
@@ -37,15 +27,24 @@ const handleChange = (evt) => {
   evt.preventDefault();
   setValues({
       ...values,
-  [evt.target.name] : evt.target.value
+      [evt.target.name] : evt.target.value
   })}
 
   const handleUpdate = (id, values) => {
     const temp = [...todos];
-    const item = temp.find(item=> item.id === id);
-    item.nombre=values
-    
+    const item = temp.find(item => item.id === id);
+    Object.assign(item, values)
     setTodos(temp)
+  }
+
+  const handleDelete = (id) => {
+    const temp = [...todos];
+    const newList = temp.filter(item => item.id !== id);
+    setTodos(newList)
+  }
+
+  const handleReset = () => {
+    setValues(dataFixed)
   }
 
     return (
@@ -54,38 +53,45 @@ const handleChange = (evt) => {
             <div className='s-conteiner'>
               <div className='s-left'>
                 {todos.map((item)=>(
-                    <Left  item = { item } key={ item.id } onUpdate={handleUpdate}/>
+                    <Left  
+                    item = { item } 
+                    key={ item.id } 
+                    onUpdate={handleUpdate}
+                    onDelete={handleDelete}
+                    />
                   ))}
                 
               </div>
               <div className='s-right'>
               <section className='right'>
                 <span>Servicio</span>
-                <form className='r-form' onSubmit={handleSubmit}>
-                  <div className='r-section'>
-                    <label htmlFor='nombre'>Nombre</label>
-                    <input 
-                    id= 'nombre'
-                    name= 'nombre'
-                    type= "text"
-                    value= {values.nombre}
-                    onChange= {handleChange}
-                    />
-                  </div>
-                  <div className='r-section'>
-                    <label htmlFor='description'>Descripción</label>
-                    <input 
-                    id= 'description'
-                    name= 'description'
-                    type= "text"
-                    value= {values.description}
-                    onChange= {handleChange} />
-                  </div>
-                  <div className='r-button'>
-                    <button type='submit' className='button' onClick={handleSubmit} >Grabar</button>
-                    <button className='button b-cancel' >Cancelar</button>
-                  </div>
-                </form>    
+                  <form className='r-form' autoComplete='off' onSubmit={handleSubmit}>
+                    <div className='r-section'>
+                      <label htmlFor='nombre'>Nombre</label>
+                      <input 
+                      disabled
+                      id= 'nombre'
+                      name= 'nombre'
+                      type= "text"
+                      value= {values.nombre}
+                      onChange= {handleChange}
+                      />
+                    </div>
+                    <div className='r-section'>
+                      <label htmlFor='description'>Descripción</label>
+                      <input 
+                      disabled
+                      id= 'description'
+                      name= 'description'
+                      type= "text"
+                      value= {values.description}
+                      onChange= {handleChange} />
+                    </div>
+                    <div className='r-button'>
+                      <button type='submit' className='button' onClick={handleSubmit}>Grabar</button>
+                      <button type='reset' className='button b-cancel' onClick={handleReset}>Cancelar</button>
+                    </div>
+                  </form>    
               </section>
               </div>
             </div> 
--- src/Components/Body/Left/Left.jsx
+++ src/Components/Body/Left/Left.jsx
@@ -2,45 +2,47 @@ import React, { useState } from 'react';
 import './Left.scss';
 
 
-const Left = ({item, onUpdate}) => {
+const Left = ({item, onUpdate, onDelete}) => {
+
+    const regNameInput = React.useRef();
+    const regDescriptionInput = React.useRef();
 
     const [isEdit, setIsEdit] = useState(false);
-
-
     
     function FormEdit() {
       const [newValue, setNewValue] = useState(item);
-
-
 
       function handleSubmit(evt){
         evt.preventDefault();
       }
 
       function handleChange(evt) {
-            evt.preventDefault();
-              const value = evt.target.value;
-              setNewValue(value)
-              console.log(value)    
-            }
-
-      function handleClickGrabar(){
-          const newData = {
-            ...item
-          }
-          setNewValue(newData)
-          console.log(newData)
-          onUpdate(item.id, newValue)
-          setIsEdit(false)
+        evt.preventDefault();
+        const value = evt.target.value;
+        setNewValue(value)
       }
+
+      function handleClickGrabar() {
+        const newData = {
+          id: item.id,
+          nombre: regNameInput.current.value,
+          description: regDescriptionInput.current.value
+        }
+        setNewValue(newData)
+        onUpdate(newData.id, newData)
+        setIsEdit(false)
+      }
+
       return(
         <form className='r-form g-form' onSubmit={handleSubmit}>
           <div className='r-alig'>
             <input 
+              ref={regNameInput}
               type="text" 
               onChange={handleChange} 
               value= {newValue.nombre}/>
-              <input 
+            <input 
+              ref={regDescriptionInput}
               type="text" 
               onChange={handleChange} 
               value= {newValue.description}   
@@ -50,17 +52,21 @@ const Left = ({item, onUpdate}) => {
         </form>
       );
     }
+
+    function deleteItem2() {
+      onDelete(item.id)
+    }
     
     function TodoElement(){
         return (
           <div className='left'>
             <div className='l-title'>
-              <span >{item.nombre}</span>
-              <span>{item.description}</span>
+              <span> { item.nombre } </span>
+              <span> { item.description } </span>
             </div>
             <div className='l-footer'>
-              <button className='l-button'onClick={() => setIsEdit(true)}>Editar</button>
-              <button className='l-button'>Eliminar</button>
+              <button className='l-button' onClick={() => setIsEdit(true)}>Editar</button>
+              <button className='l-button' onClick={deleteItem2}>Eliminar</button>
             </div>
           </div>
       );
@@ -71,8 +77,6 @@ const Left = ({item, onUpdate}) => {
       {isEdit ? <FormEdit /> : <TodoElement/>} 
       </div>
     )
-
- 
     
 }
 
